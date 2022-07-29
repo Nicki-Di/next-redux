1eb5e8b05d864d22ab2893a84879d0df03db8514
Nicky <78273681+Nicki-Di@users.noreply.github.com>
2022-07-29 14:48:37 +0430

    Update dashboard.js

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -37,6 +37,7 @@ export default function Layer() {
     return (
         <>
             <div>
+                {/* Hamburger sidebar for mobile */}
                 <Transition.Root show = {sidebarOpen} as = {Fragment}>
                     <Dialog as = "div" className = "fixed inset-0 flex z-40 md:hidden" onClose = {setSidebarOpen}>
                         <Transition.Child
@@ -120,44 +121,45 @@ export default function Layer() {
                     </Dialog>
                 </Transition.Root>
 
-                {/* Static sidebar for desktop */}
-                <div className = "hidden md:flex md:flex-col md:fixed md:inset-y-0 w-[15%] shadow-md z-10">
-                    {/* Sidebar */}
-                    <div className = "flex flex-col flex-grow pt-5 overflow-y-auto bg-s-100 ">
-                        <div className = "flex items-center flex-shrink-0 px-4">
-                            <img
-                                className = "h-8 w-auto"
-                                src = "https://tailwindui.com/img/logos/workflow-logo-indigo-600-mark-gray-800-text.svg"
-                                alt = "Workflow"
-                            />
-                        </div>
-                        <div className = "mt-5 flex-grow flex flex-col">
-                            <nav className = "flex-1 px-2 pb-4 space-y-1">
-                                {navigation.map((item) => (
-                                    <a
-                                        key = {item.name}
-                                        href = {item.href}
-                                        className = {classNames(
-                                            item.current ? 'bg-gray-100 text-gray-900' : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900',
-                                            'group flex items-center px-2 py-2 text-sm font-medium rounded-md'
-                                        )}
-                                    >
-                                        <item.icon
-                                            className = {classNames(
-                                                item.current ? 'text-gray-500' : 'text-gray-400 group-hover:text-gray-500',
-                                                'mr-3 flex-shrink-0 h-6 w-6'
-                                            )}
-                                            aria-hidden = "true"
-                                        />
-                                        {item.name}
-                                    </a>
-                                ))}
-                            </nav>
-                        </div>
+
+                {/* Static sidebar for mobile */}
+
+                {/* Sidebar */}
+                <div
+                    className = "hidden md:flex md:flex-col md:fixed md:inset-y-0 w-[15%] shadow-md z-10 bg-s-100 items-center justify-between pt-5 overflow-y-auto ">
+                    <div className = "flex items-center flex-shrink-0 px-4">
+                        <img
+                            className = "h-8 w-auto"
+                            src = "/dashboard/layer-icon.png"
+                            alt = "Workflow"
+                        />
                     </div>
+                        <nav className = "mt-5 flex-grow flex flex-col justify-center gap-12 px-2 pb-4 space-y-1">
+                            {navigation.map((item) => (
+                                <a
+                                    key = {item.name}
+                                    href = {item.href}
+                                    className = {classNames(
+                                        item.current ? 'bg-gray-100 text-gray-900' : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900',
+                                        'group flex items-center px-2 py-2 text-sm font-medium rounded-md'
+                                    )}
+                                >
+                                    <item.icon
+                                        className = {classNames(
+                                            item.current ? 'text-gray-500' : 'text-gray-400 group-hover:text-gray-500',
+                                            'mr-3 flex-shrink-0 h-6 w-6'
+                                        )}
+                                        aria-hidden = "true"
+                                    />
+                                    {item.name}
+                                </a>
+                            ))}
+                        </nav>
                 </div>
+
                 {/*header + main*/}
-                <div className = "md:w-[85%] flex flex-col flex-1 mr-auto">
+                <div className = "md:w-[85%] flex flex-col mr-auto">
+                    {/*header*/}
                     <div className = "sticky top-0 flex-shrink-0 flex h-16 bg-s-100">
                         <button
                             type = "button"
@@ -181,8 +183,8 @@ export default function Layer() {
                             </div>
                         </div>
                     </div>
-
-                    <main className = "flex-1 bg-s-90 min-h-screen">
+                    {/*main*/}
+                    <main className = "bg-s-90 min-h-screen">
                         <div className = "py-6">
                             <div className = "max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
                                 <h1 className = "text-2xl font-semibold text-gray-900">Dashboard</h1>
